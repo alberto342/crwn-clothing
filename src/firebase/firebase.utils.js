9af63e3bcdfdc493ac6c1d838f679f65bf9ad5ef
Albert <albert.brami@gmail.com>
2019-11-29 10:47:52 +0200

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included theSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,41 +3,74 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyC0KM0Wx0rYTVFia8TrNtpwCc-4-KG1TF4",
-  authDomain: "crwn-db-377f6.firebaseapp.com",
-  databaseURL: "https://crwn-db-377f6.firebaseio.com",
-  projectId: "crwn-db-377f6",
-  storageBucket: "crwn-db-377f6.appspot.com",
-  messagingSenderId: "300991500782",
-  appId: "1:300991500782:web:27e3dd0d621acd0d62beff",
-  measurementId: "G-PGWYKQE68X"
+    apiKey: "AIzaSyC0KM0Wx0rYTVFia8TrNtpwCc-4-KG1TF4",
+    authDomain: "crwn-db-377f6.firebaseapp.com",
+    databaseURL: "https://crwn-db-377f6.firebaseio.com",
+    projectId: "crwn-db-377f6",
+    storageBucket: "crwn-db-377f6.appspot.com",
+    messagingSenderId: "300991500782",
+    appId: "1:300991500782:web:27e3dd0d621acd0d62beff",
+    measurementId: "G-PGWYKQE68X"
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+    const snapShot = await userRef.get();
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      });
-    } catch (error) {
-      console.log('error creating user', error.message);
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
     }
-  }
 
-  return userRef;
+    return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
 };
 
 export const auth = firebase.auth();
