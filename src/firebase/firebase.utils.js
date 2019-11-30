2dff20db7ca46bece3c73eb96f94279fa4f60d57
Albert <albert.brami@gmail.com>
2019-11-30 15:23:17 +0200

    created user and cart sagas for sign in, sign out and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -23,7 +23,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     const snapShot = await userRef.get();
 
     if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
+        const {displayName, email} = userAuth;
         const createdAt = new Date();
         try {
             await userRef.set({
@@ -51,13 +51,12 @@ export const addCollectionAndDocuments = async (
         const newDocRef = collectionRef.doc();
         batch.set(newDocRef, obj);
     });
-
     return await batch.commit();
 };
 
 export const convertCollectionsSnapshotToMap = collections => {
     const transformedCollection = collections.docs.map(doc => {
-        const { title, items } = doc.data();
+        const {title, items} = doc.data();
 
         return {
             routeName: encodeURI(title.toLowerCase()),
@@ -73,11 +72,21 @@ export const convertCollectionsSnapshotToMap = collections => {
     }, {});
 };
 
+export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            unsubscribe();
+            resolve(userAuth);
+
+        }, reject())
+    })
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,19 @@
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {createStructuredSelector} from "reselect";
+
+import {selectIsCollectionsLoaded} from "../../redux/shop/shop.selectors";
+import Withspinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPage from './collection.component';
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: (state) => !selectIsCollectionsLoaded(state)
+});
+
+
+const CollectionPageContainer = compose(
+    connect(mapStateToProps),
+    Withspinner
+)(CollectionPage);
+
+export default CollectionPageContainer;
